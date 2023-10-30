2616175bb43c388add61bab9ac08e6a493f912c2
yuyao <asas12365414@gmail.com>
2023-10-30 12:06:03 +0800

    新增product的crud

---
+++ src/main/java/com/woof/product/dao/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.woof.product.dao;
+
+import com.woof.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByProdName(String prodName);
+
+}
--- src/main/java/com/woof/product/service/ProductService.java
+++ src/main/java/com/woof/product/service/ProductService.java
@@ -1,60 +1,44 @@
 package com.woof.product.service;
 
-import java.util.List;
-
-import com.woof.product.dao.ProductDAO;
-import com.woof.product.dao.ProductDAOImpl;
 import com.woof.product.entity.Product;
+import com.woof.product.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
 
-    private ProductDAO dao;
+    @Autowired
+    private ProductRepository repository;
 
-    public ProductService() {
-        dao = new ProductDAOImpl();
+    public Product saveProduct(Product product) {
+        return repository.save(product);
     }
 
-    public Product addProduct(Integer prodNo, Integer prodCatNo, String prodContent, Integer prodPrice, String prodName, Boolean prodStatus) {
-
-        Product product = new Product();
-
-        product.setProdNo(prodNo);
-        product.setProdCatNo(prodCatNo);
-        product.setProdContent(prodContent);
-        product.setProdPrice(prodPrice);
-        product.setProdName(prodName);
-        product.setProdStatus(prodStatus);
-        dao.insert(product);
-
-        return product;
+    public List<Product> saveProducts(List<Product> products) {
+        return repository.saveAll(products);
     }
 
-    public Product updateProduct(Integer prodNo, Integer prodCatNo, String prodContent, Integer prodPrice, String prodName, Boolean prodStatus) {
-
-        Product product = new Product();
-
-        product.setProdNo(prodNo);
-        product.setProdCatNo(prodCatNo);
-        product.setProdContent(prodContent);
-        product.setProdPrice(prodPrice);
-        product.setProdName(prodName);
-        product.setProdStatus(prodStatus);
-        dao.update(product);
-
-        return product;
+    public List<Product> getProducts() {
+        return repository.findAll();
     }
 
-    public void deleteProduct(Integer prodNo) {
-        dao.delete(prodNo);
+    public Optional<Product> getProductById(int id) {
+        return repository.findById(id);
     }
 
-    public Product getOneProduct(Integer prodNo) {
-        return dao.findByProdNo(prodNo);
+    public String deleteProduct(int id) {
+        repository.deleteById(id);
+        return "Product removed: " + id;
     }
 
-    public List<Product> getAll() {
-        return dao.getAll();
+    public Product updateProduct(Product product) {
+        Product existingProduct = repository.findById(product.getProdNo()).orElse(null);
+        existingProduct.setProdName(product.getProdName());
+        existingProduct.setProdPrice(product.getProdPrice());
+        return repository.save(existingProduct);
     }
-
-	
 }
