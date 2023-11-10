08f3e01752c2604294df4156cd42a064080487ef
yuyao <asas12365414@gmail.com>
2023-11-10 18:54:54 +0800

    刪除不必要的檔案

--- src/main/java/com/woof/returnlist/dao/ReturnListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.woof.returnlist.dao;
-
-import com.woof.returnlist.entity.ReturnList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReturnListRepository extends JpaRepository<ReturnList, Integer> {
-}
