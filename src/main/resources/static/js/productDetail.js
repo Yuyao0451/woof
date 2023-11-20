f676025fb6e147805503b8d33fd1df128ab7457c
yuyao <asas12365414@gmail.com>
2023-11-21 00:23:47 +0800

    修改促銷商品詳情 新增促銷價與活動名稱和內容

--- src/main/resources/static/js/productDetail.js
+++ src/main/resources/static/js/productDetail.js
@@ -8,23 +8,38 @@ $(document).ready(function() {
             url: `/productById/${prodNo}`,
             type: 'GET',
             success: function(product) {
-                // 更新頁面上的商品信息
                 $('#product-image').attr('src', `/productImage/${product.prodNo}`);
                 $('#product-name').text(product.prodName);
                 $('#product-description').text(product.prodContent);
-                $('#product-price').text(`$${product.prodPrice}`);
 
-                // 更新加入購物車按鈕的 data-* 屬性
-                $('.add-to-cart').attr('data-id', product.prodNo);
-                $('.add-to-cart').attr('data-name', product.prodName);
-                $('.add-to-cart').attr('data-price', product.prodPrice);
+                if (product.promotion && product.promoId) {
+                    $('#product-price').hide();
+                    // 如果是促銷商品，顯示促銷價格和原價
+                    $('#product-original-price').text(`原價: $${product.prodPrice}`).css('text-decoration', 'line-through').show();
+                    $('#product-promo-price').text(`促銷價: $${product.promoPrice}`).addClass('text-danger');
 
-                console.log($('.add-to-cart').attr('data-id'));
-                console.log($('.add-to-cart').attr('data-name'));
-                console.log($('.add-to-cart').attr('data-price'));
+                    // 獲取促銷活動的詳細信息
+                    $.ajax({
+                        url: `/promotion/${product.promoId}`,
+                        type: 'GET',
+                        success: function(promotion) {
+                            $('#promo-details').html(`促銷活動: ${promotion.paName}<br>內容: ${promotion.paContent}`);
+                        }
+                    });
+                } else {
+                    $('#product-price').text(`價格: $${product.prodPrice}`).show();
+                    // 非促銷商品，隱藏促銷信息
+                    $('#product-original-price, #product-promo-price, #promo-details').hide();
+                }
+
+                $('.add-to-cart').attr({
+                    'data-id': product.prodNo,
+                    'data-name': product.prodName,
+                    'data-price': product.promotion ? product.promoPrice : product.prodPrice
+                });
             },
             error: function(error) {
-                console.log('Error fetching product details:', error);
+                console.error("錯誤信息:", error);
             }
         });
     }
